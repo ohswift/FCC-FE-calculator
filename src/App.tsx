a8559d937bb4d4d34371eef618ec55782a6879c6
vincent <mightyme@qq.com>
2023-02-23 02:08:41 +0800

    feat: pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,258 +1,279 @@
-import React, { ChangeEvent, CSSProperties } from "react";
+import React, {
+  ChangeEvent,
+  MouseEvent,
+  CSSProperties,
+  MouseEventHandler,
+} from "react";
 
-interface BankType {
-  keyCode: string;
-  id: string;
-  url: string;
-}
-
-const bankOne: BankType[] = [
-  {
-    keyCode: "Q",
-    id: "Heater-1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
-  },
-  {
-    keyCode: "W",
-    id: "Heater-2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
-  },
-  {
-    keyCode: "E",
-    id: "Heater-3",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
-  },
-  {
-    keyCode: "A",
-    id: "Heater-4",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
-  },
-  {
-    keyCode: "S",
-    id: "Clap",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
-  },
-  {
-    keyCode: "D",
-    id: "Open-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
-  },
-  {
-    keyCode: "Z",
-    id: "Kick-n'-Hat",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
-  },
-  {
-    keyCode: "X",
-    id: "Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
-  },
-  {
-    keyCode: "C",
-    id: "Closed-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
-  },
+const DOTS = ["decimal", "."];
+const CLEARS_DATA = ["clear", "AC"];
+const NUMBERS_DATA = [
+  ["seven", "7"],
+  ["eight", "8"],
+  ["nine", "9"],
+  ["four", "4"],
+  ["five", "5"],
+  ["six", "6"],
+  ["one", "1"],
+  ["two", "2"],
+  ["three", "3"],
+  ["zero", "0"],
+];
+const OPERATORS_DATA = [
+  ["divide", "/"],
+  ["multiply", "*"],
+  ["subtract", "-"],
+  ["add", "+"],
+  ["equals", "="],
 ];
 
-const bankTwo: BankType[] = [
-  {
-    keyCode: "Q",
-    id: "Chord-1",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
-  },
-  {
-    keyCode: "W",
-    id: "Chord-2",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
-  },
-  {
-    keyCode: "E",
-    id: "Chord-3",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
-  },
-  {
-    keyCode: "A",
-    id: "Shaker",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
-  },
-  {
-    keyCode: "S",
-    id: "Open-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
-  },
-  {
-    keyCode: "D",
-    id: "Closed-HH",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
-  },
-  {
-    keyCode: "Z",
-    id: "Punchy-Kick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
-  },
-  {
-    keyCode: "X",
-    id: "Side-Stick",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
-  },
-  {
-    keyCode: "C",
-    id: "Snare",
-    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
-  },
-];
+const isOperator = /[*/+-]/;
+const endsWithOperator = /([*+-/])$/;
+const endsWithNegativeSign = /(\d)([*/+-]{1}-)$/;
+const operators = ["+", "-", "*", "/"];
 
-const activeStyle: React.CSSProperties = {
-  backgroundColor: "orange",
-  boxShadow: "0 3px black",
-  height: 77,
-  marginTop: 13,
-};
-
-const inactiveStyle: React.CSSProperties = {
-  backgroundColor: "#74b9ff",
-  marginTop: 10,
-  boxShadow: "3px 3px 5px black",
-};
-
-interface DrumPadProp {
-  keyCode: string;
-  clipId: string;
-  clip: string;
-  updateDisplay: (s: string) => void;
-}
-
-interface DrumPadState {
-  padStyle?: React.CSSProperties;
-  actived: boolean;
-}
-
-class DrumPad extends React.Component<DrumPadProp, DrumPadState> {
-  constructor(props: DrumPadProp) {
-    super(props);
-    this.state = {
-      actived: false,
-    };
-  }
-  componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress);
-  }
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.handleKeyPress);
-  }
-  handleKeyPress = (e: KeyboardEvent) => {
-    let key = "";
-    if (e.key) {
-      key = e.key.toUpperCase();
-    }
-    if (key === this.props.keyCode) {
-      this.playSound();
-    }
-  };
-  toggleActive = () => {
-    this.setState((prevState, props) => {
-      return { actived: !prevState.actived };
-    });
-  };
-  playSound = () => {
-    const sound: HTMLAudioElement = document.getElementById(
-      this.props.keyCode
-    ) as HTMLAudioElement;
-    sound.currentTime = 0;
-    sound.play();
-    this.toggleActive();
-    // sound.addEventListener("ended", ()=>{})
-    setTimeout(() => this.toggleActive(), 100);
-    this.props.updateDisplay(this.props.clipId.replace(/-/g, " "));
-  };
+class Output extends React.Component<{
+  displayValue: string;
+  expression: string;
+}> {
   render() {
     return (
-      <div
-        id={this.props.clipId}
-        onClick={this.playSound}
-        className={this.state.actived ? "drum-pad actived" : "drum-pad"}
-        // className={"drum-pad actived"}
-        // style={this.state.padStyle}
-      >
-        <audio
-          className="clip"
-          id={this.props.keyCode}
-          src={this.props.clip}
-        ></audio>
-        {this.props.keyCode}
-      </div>
+      <>
+        <p className="expression">{this.props.expression}</p>
+        <div className="display" id="display">
+          {this.props.displayValue}
+        </div>
+      </>
     );
   }
 }
 
-interface PadBankState {
-  padStyle: React.CSSProperties;
+interface ButtonsProps {
+  handleClick: MouseEventHandler;
 }
-interface PadBankProp {
-  currentPadBank: BankType[];
-  updateDisplay: (a: string) => void;
-}
-class PadBank extends React.Component<PadBankProp, PadBankState> {
-  constructor(props: PadBankProp) {
-    super(props);
-  }
+
+class Buttons extends React.Component<ButtonsProps> {
   render() {
-    let padBank = this.props.currentPadBank.map((drumObj, i) => {
-      return (
-        <DrumPad
-          clipId={drumObj.id}
-          key={i}
-          clip={drumObj.url}
-          keyCode={drumObj.keyCode}
-          updateDisplay={this.props.updateDisplay}
-        />
-      );
-    });
-    return <div className="pad-bank">{padBank}</div>;
+    return (
+      <>
+        <div className="button-container left">
+          <button
+            className="light clear"
+            id={CLEARS_DATA[0]}
+            onClick={this.props.handleClick}
+          >
+            {CLEARS_DATA[1]}
+          </button>
+          {NUMBERS_DATA.map((num) => (
+            <button
+              className={num[1] == "0" ? "big dark" : "dark"}
+              id={num[0]}
+              key={num[0]}
+              onClick={this.props.handleClick}
+            >
+              {num[1]}
+            </button>
+          ))}
+          <button
+            className="dark"
+            id={DOTS[0]}
+            onClick={this.props.handleClick}
+          >
+            {DOTS[1]}
+          </button>
+        </div>
+        <div className="button-container right">
+          {OPERATORS_DATA.map((ops) => (
+            <button
+              className="orange"
+              id={ops[0]}
+              key={ops[0]}
+              onClick={this.props.handleClick}
+            >
+              {ops[1]}
+            </button>
+          ))}
+        </div>
+      </>
+    );
   }
 }
 
 interface AppState {
-  display: string;
-  currentPadBank: BankType[];
-  currentPadBankId: string;
-  sliderVal: number;
+  displayValue: string;
+  expression: string;
+  evaluated: boolean; // showing last evaluated result(aka: xxx=xxx)
+  prevVal: string; // value before operator(easy to use)
 }
 
 class App extends React.Component<{}, AppState> {
+  state: AppState = {
+    displayValue: "0",
+    expression: "", // invalid input
+    evaluated: false,
+    prevVal: "",
+  };
+
   constructor(props: {}) {
     super(props);
-    this.state = {
-      display: String.fromCharCode(160),
-      currentPadBank: bankOne,
-      currentPadBankId: "Heater Kit",
-      sliderVal: 0.3,
-    };
   }
 
-  displayClipName = (name: string) => {
+  handleEvaluate = () => {
+    let { expression, displayValue, evaluated, prevVal } = this.state;
+    if (evaluated) return;
+    if (prevVal === "") return; // no operators
+    const res = Math.round(eval(expression) * 100000) / 100000;
     this.setState({
-      display: name,
+      expression: expression + `=${res}`,
+      displayValue: `${res}`,
+      evaluated: true,
+      prevVal: "",
+    });
+    return;
+  };
+
+  handleOperators = (text: string) => {
+    let { expression, displayValue, evaluated, prevVal } = this.state;
+
+    // has evaluated
+    if (evaluated) {
+      this.setState({
+        prevVal: displayValue,
+        expression: displayValue + text,
+        displayValue: text,
+        evaluated: false,
+      });
+      return;
+    }
+    // check if double operator
+    if (!endsWithOperator.test(expression)) {
+      this.setState({
+        displayValue: text,
+        prevVal: expression,
+        expression: expression + text,
+      });
+      return;
+    }
+    if (!endsWithNegativeSign.test(expression)) {
+      // expression.replace(endsWithOperator, `$1 ${text}`);
+      this.setState({
+        displayValue: text,
+        expression: (text === "-" ? expression : prevVal) + text,
+      });
+      return;
+    }
+    // ends with negative sign
+    if (text != "-") {
+      // replace +- with (*/+)
+      // expression.replace(endsWithNegativeSign, `$1 ${text}`);
+      this.setState({
+        displayValue: text,
+        expression: prevVal + text,
+      });
+    }
+  };
+
+  handleDot = () => {
+    let { expression, displayValue, evaluated } = this.state;
+    if (evaluated) {
+      this.setState({
+        displayValue: "0.",
+        expression: "0.",
+        evaluated: false,
+      });
+      return;
+    }
+    // 最后一个值是否已经有.了
+    if (displayValue.indexOf(".") == -1) {
+      this.setState({
+        displayValue: displayValue + ".",
+        expression: expression + ".",
+      });
+    }
+  };
+
+  handleNumbers = (text: string) => {
+    let { expression, displayValue, evaluated, prevVal } = this.state;
+    if (evaluated) {
+      this.setState({
+        displayValue: text,
+        expression: text,
+        evaluated: false,
+      });
+      return;
+    }
+
+    // need to remove illegal 0
+    if (prevVal === "") {
+      // is inputing first hand
+      if (expression.length == 1 && expression[0] === "0") {
+        expression = "";
+      }
+    } else {
+      // prevVal+operator+"0"+text
+      if (
+        expression.length == prevVal.length + 2 &&
+        expression.slice(-1) === "0"
+      ) {
+        expression = expression.slice(0, -1);
+      }
+    }
+    expression += text;
+    if (isOperator.test(displayValue) || displayValue === "0") {
+      displayValue = text;
+    } else {
+      displayValue += text;
+    }
+    this.setState({
+      expression: expression,
+      displayValue: displayValue,
+      evaluated: false,
     });
   };
 
-  clearDisplay = () => {
-    this.setState({
-      display: String.fromCharCode(160),
-    });
+  handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+    const text = (e.target as HTMLButtonElement).innerHTML;
+    let { expression, displayValue, evaluated } = this.state;
+    const lastChar = expression.slice(-1) || "";
+
+    // 0. handle AC
+    if (text === "AC") {
+      this.setState({
+        displayValue: "0",
+        expression: "",
+        evaluated: false,
+        prevVal: "",
+      });
+      return;
+    }
+
+    // 1. handle if click equal
+    if (text == "=") {
+      return this.handleEvaluate();
+    }
+    // 2. handle operators
+    if (isOperator.test(text)) {
+      return this.handleOperators(text);
+    }
+    // 3. handle numbers
+    if (text == ".") {
+      return this.handleDot();
+    }
+    return this.handleNumbers(text);
   };
+
   render() {
     return (
-      <div id="drum-machine" className="inner-container">
-        <PadBank
-          updateDisplay={this.displayClipName}
-          currentPadBank={this.state.currentPadBank}
-        />
-
-        <div className="controls-container">
-          <p id="display">{this.state.display}</p>
+      <>
+        {/* <p>{JSON.stringify(this.state)}</p> */}
+        <div className="container">
+          <Output
+            displayValue={this.state.displayValue}
+            expression={this.state.expression}
+          ></Output>
+          <Buttons handleClick={this.handleClick}></Buttons>
         </div>
-      </div>
+      </>
     );
   }
 }
